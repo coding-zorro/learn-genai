6991cc5b0680de506025c101345c15c1a47e86db
user <user@example.com>
2025-07-02 09:11:44 +0530

    Initial commit

---
+++ first.py
@@ -0,0 +1,40 @@
+from langchain_ollama import OllamaLLM
+from pydantic import BaseModel
+from langchain_core.output_parsers import JsonOutputParser
+from langchain_core.prompts import PromptTemplate
+
+
+llm = OllamaLLM(model="mistral")
+
+result = llm.invoke("tell me about mysore's history")
+print(result)
+
+
+class Person(BaseModel):
+    name: str
+    age: int
+    location: str
+
+
+parser = JsonOutputParser(pydantic_object=Person)
+
+
+prompt = PromptTemplate.from_template(
+    """
+        You are an information extractor AI. Given the following input text, extract the person's name, age and location.
+
+        Respond ONLY with the result in **valid JSON** format — no commentary, no explanation, no markdown.
+        Respond only in the following JSON format:
+
+        {format_instructions}
+
+        Text:
+        {text}
+    """
+)
+
+chain = prompt | llm | parser
+input = {"format_instructions": parser.get_format_instructions,
+         "text": "John Doe is a 34-year-old software engineer. His is from Los Angeles"}
+result = chain.invoke(input)
+print(result)
